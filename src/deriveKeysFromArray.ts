ba847bcc77c7533d8119f3520e820863786d1175
David Johnston <david@blacksheepcode.com>
2020-01-03 15:22:24 +1100

    Add one example

---
+++ src/deriveKeysFromArray.ts
@@ -0,0 +1,30 @@
+//https://stackoverflow.com/questions/59572807/convert-an-array-of-one-type-into-a-mapped-object-of-that-type/59573070#59573070
+
+type Foo = {
+    name: string;
+    value: any;
+}
+
+const myFoos = [
+    {
+        name: "hello",
+        value: 1,
+    },
+    {
+        name: "world",
+        value: "bar"
+    }
+] as const;
+
+
+function createObjectFromFooArray<T extends Foo> (foos: readonly T[])  {  //Need to change the return type
+
+
+    return foos.reduce((acc, cur) => {
+        return {
+            ...acc,
+            [cur.name]: cur
+        } 
+    }, {} as { [K in T["name"]]: Extract<T, { name: K }>;
+}
+
